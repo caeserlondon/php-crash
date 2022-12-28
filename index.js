5e7e82685ff25dd95c9e92345f189cf93ff610d1
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-12-28 11:45:40 +0000

    Updating

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 [This is writen in pseudocode]
 if(number is even) number = number / 2
-if(number is odd) number = 3*number + 1  
+if (number is odd) number = 3 * number + 1  
+
+
+hotpo(1) returns 0
+(1 is already 1)
+
+hotpo(5) returns 5
+5 -> 16 -> 8 -> 4 -> 2 -> 1
+
+hotpo(6) returns 8
+6 -> 3 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1
+
+hotpo(23) returns 15
+23 -> 70 -> 35 -> 106 -> 53 -> 160 -> 80 -> 40 -> 20 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1
